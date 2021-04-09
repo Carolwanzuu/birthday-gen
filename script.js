344ee92d198c4f887c30cbe02a3a85928cd08278
Carolwanzuu <carolkyalo01@gmail.com>
2021-04-09 20:40:54 +0300

    added an object on the name of males

---
+++ script.js
@@ -0,0 +1,31 @@
+let male = function(){
+    Sunday: Kwasi
+
+    Monday: Kwadwo
+
+    Tuesday: Kwabena
+
+    Wednesday: Kwaku
+
+    Thursday:  Yaw
+
+    Friday: Kofi
+
+    Saturday: Kwame
+};
+
+let female = function(){
+    Sunday: Akosua
+
+    Monday: Adwoa
+
+    Tuesday: Abenaa
+
+    Wednesday: Akua
+
+    Thursday:  Yaa
+
+    Friday: Afua
+
+    Saturday: Ama
+};
