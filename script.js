f3cc0fb44c99955df9754414e55139aa1c350128
Carolwanzuu <carolkyalo01@gmail.com>
2021-04-13 12:40:36 +0300

    edited the radio button details

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let male_names = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku" "Yaw", "Kofi", "Kwame"];
-let female_names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-let year_of_birth, YY, CC, MM, DD, day_of_week;
-let gender;
-let result_data = document.getElementById('result');
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let yearOfBirth, YY, CC, MM, DD, d;
+//let gender;
+//let result_data = document.getElementById('result');
+
 
 
 
@@ -13,23 +14,28 @@ function getBirthday(){
     YY = year_of_birth.slice(0,2);
     CC = year_of_birth.slice(2, 4);
     MM = parseInt(document.getElementById("month").value);
-    DD = parseInt(document.getElementById("day").value);
+    DD = parseInt(document.getElementById("date").value);
+    
+    d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
 
+    console.log(d);
+    return Math.floor(d);
+}
+    // console.log(YY);
+    // console.log(CC);
+    // console.log(MM);
+    // console.log(DD);
+   
 
-    console.log(YY);
-    console.log(CC);
-    console.log(MM);
-    console.log(DD);
-
-    if (DD <= 0 || DD > 31)
-        alert("Enter a valid one")
-    }else if(MM <O || MM > 12){
-        alert("enter a valid month")
-    }else{
-       alert("confirm inputs");
+    if (DD <= 0 || DD > 31){
+        alert("Enter a valid one");
+    }else if(MM <=0 || MM > 12){
+        alert("enter a valid month");
+     }else{
+    alert("confirm inputs");
     };
 
-let day_of_the_week = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+//let day_of_the_week(d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 
 //  CC - is the century digits. For example 1989 has CC = 19
 
@@ -41,59 +47,27 @@ let day_of_the_week = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD 
 
 //  mod - is the modulus function ( % )
 
-function getAkanName(){
-    getBirthday();
-    day_of_week = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 ;
-    day_of_week = Math.ceil(day_of_week);
-    console.log(`Day of the week: ${day_of_week}`);
-    checkGender();
+function getAkanName(Birthday){
+    //getBirthday();
+   // day_of_week = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 ;
+    //day_of_week = Math.floor(day_of_week);
+    let genderRadio = document.getElementsByName("gender");
+    //console.log("gender");
+    for(i = 0; i<genderRadio.length; i++){
+        if(genderRadio[i].checked)
+            gender = genderRadio[i].value;
+        }
+    
+
+if(gender == male){
+    alert("You were born on " + weekdays[Birthday] + " you are " + gender + " and your Akan name is " + maleNames[Birthday] + "!" );
+}else{
+    alert("You were born on " + weekdays[Birthday] + " you are " + gender + " and your Akan name is " + femaleNames[Birthday] + "!" );
 }
-
-function checkGender(){
-    let radios = document.getElementsByName("radio");
-    if(radios[0].checked == true) {
-        gender = "Male";
-    }else if(radios[1].checked == true){
-        gender = "Female";
-    }else{
-        alert("Any other?");
-    }
+}
+    function getName(){
+     Birthday = getBirthday();
+     getAkanName(Birthday);
+ }
 
 
-    if(gender == "Male"){
-        if(day_of_week == 0){
-            result_data.innerHTML = `The day of the week is ${weekdays[0]} and your Akan name is ${male_names[0]}`;
-        }else if(day_of_week == 1||day_of_week == -1){
-            result_data.innerHTML = `The day of the week is ${weekdays[1]} and your Akan name is ${male_names[1]}`;
-        }else if(day_of_week = 2 || day_of_week == -2){
-            result_data.innerHTML = `The day of the week is ${weekdays[2]} and your Akan name is ${male_names[2]}`;
-        }else if(day_of_week = 3 || day_of_week == -3){
-            result_data.innerHTML = `The day of the week is ${weekdays[3]} and your Akan name is ${male_names[3]}`;
-        }else if(day_of_week = 4 || day_of_week == -4){
-            result_data.innerHTML = `The day of the week is ${weekdays[4]} and your Akan name is ${male_names[4]}`;
-        }else if(day_of_week = 5 || day_of_week == -5){
-            result_data.innerHTML = `The day of the week is ${weekdays[5]} and your Akan name is ${male_names[5]}`;
-        }else if(day_of_week = 6 || day_of_week == -6){
-            result_data.innerHTML = `The day of the week is ${weekdays[6]} and your Akan name is ${male_names[6]}`;
-        }else if(day_of_week = 7 || day_of_week == -7){
-            result_data.innerHTML = `The day of the week is ${weekdays[7]} and your Akan name is ${male_names[7]}`;
-        }
-    }else if(gender = "female"){
-        if(day_of_week == 0){
-            result_data.innerHTML = `The day of the week is ${weekdays[0]} and your Akan name is ${female_names[0]}`;
-        }else if(day_of_week == 1 ||day_of_week == -1){
-            result_data.innerHTML = `The day of the week is ${weekdays[1]} and your Akan name is ${female_names[1]}`;
-        }else if(day_of_week = 2 || day_of_week == -2)
-            result_data.innerHTML = `The day of the week is ${weekdays[2]} and your Akan name is ${female_names[2]}`;
-        }else if(day_of_week = 3 || day_of_week == -3){
-            result_data.innerHTML = `The day of the week is ${weekdays[3]} and your Akan name is ${female_names[3]}`;
-        }else if(day_of_week = 4 || day_of_week == -4){
-            result_data.innerHTML = `The day of the week is ${weekdays[4]} and your Akan name is ${female_names[4]}`;
-        }else if(day_of_week = 5 || day_of_week == -5){
-            result_data.innerHTML = `The day of the week is ${weekdays[5]} and your Akan name is ${female_names[5]}`;
-        }else if(day_of_week = 6 || day_of_week == -6){
-            result_data.innerHTML = `The day of the week is ${weekdays[6]} and your Akan name is ${female_names[6]}`;
-        }else if(day_of_week = 7 || day_of_week == -7){
-            result_data.innerHTML = `The day of the week is ${weekdays[7]} and your Akan name is ${female_names[7]}`;
-        }
-    }
