4d67d19eaf08f3f557091ba9d23849deb9aedad8
Carolwanzuu <carolkyalo01@gmail.com>
2021-04-13 10:22:39 +0300

    edited the button label

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getBirthday(){
     YY = year_of_birth.slice(0,2);
     CC = year_of_birth.slice(2, 4);
     MM = parseInt(document.getElementById("month").value);
-    DD = parseInt(document.getElementById("day_of_month").value);
+    DD = parseInt(document.getElementById("day").value);
 
 
     console.log(YY);
@@ -26,10 +26,8 @@ function getBirthday(){
     }else if(MM <O || MM > 12){
         alert("enter a valid month")
     }else{
-        "confirm inputs"
-    }
-};
-
+       alert("confirm inputs");
+    };
 
 let day_of_the_week = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 
@@ -46,7 +44,7 @@ let day_of_the_week = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD 
 function getAkanName(){
     getBirthday();
     day_of_week = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 ;
-    day_of_week = Math.round(day_of_week);
+    day_of_week = Math.ceil(day_of_week);
     console.log(`Day of the week: ${day_of_week}`);
     checkGender();
 }
@@ -57,45 +55,45 @@ function checkGender(){
         gender = "Male";
     }else if(radios[1].checked == true){
         gender = "Female";
-    }else(
-        alert()
-    )
+    }else{
+        alert("Any other?");
+    }
 
 
     if(gender == "Male"){
         if(day_of_week == 0){
-            result_data.innerHTML = `The day of the week is ${weekdays[0]} and your Akan name is ${masculine_names[0]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[0]} and your Akan name is ${male_names[0]}`;
         }else if(day_of_week == 1||day_of_week == -1){
-            result_data.innerHTML = `The day of the week is ${weekdays[1]} and your Akan name is ${masculine_names[1]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[1]} and your Akan name is ${male_names[1]}`;
         }else if(day_of_week = 2 || day_of_week == -2){
-            result_data.innerHTML = `The day of the week is ${weekdays[2]} and your Akan name is ${masculine_names[2]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[2]} and your Akan name is ${male_names[2]}`;
         }else if(day_of_week = 3 || day_of_week == -3){
-            result_data.innerHTML = `The day of the week is ${weekdays[3]} and your Akan name is ${masculine_names[3]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[3]} and your Akan name is ${male_names[3]}`;
         }else if(day_of_week = 4 || day_of_week == -4){
-            result_data.innerHTML = `The day of the week is ${weekdays[4]} and your Akan name is ${masculine_names[4]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[4]} and your Akan name is ${male_names[4]}`;
         }else if(day_of_week = 5 || day_of_week == -5){
-            result_data.innerHTML = `The day of the week is ${weekdays[5]} and your Akan name is ${masculine_names[5]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[5]} and your Akan name is ${male_names[5]}`;
         }else if(day_of_week = 6 || day_of_week == -6){
-            result_data.innerHTML = `The day of the week is ${weekdays[6]} and your Akan name is ${masculine_names[6]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[6]} and your Akan name is ${male_names[6]}`;
         }else if(day_of_week = 7 || day_of_week == -7){
-            result_data.innerHTML = `The day of the week is ${weekdays[7]} and your Akan name is ${masculine_names[7]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[7]} and your Akan name is ${male_names[7]}`;
         }
-    }else if(gender = 'female'){
+    }else if(gender = "female"){
         if(day_of_week == 0){
-            result_data.innerHTML = `The day of the week is ${weekdays[0]} and your Akan name is ${feminine_names[0]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[0]} and your Akan name is ${female_names[0]}`;
         }else if(day_of_week == 1 ||day_of_week == -1){
-            result_data.innerHTML = `The day of the week is ${weekdays[1]} and your Akan name is ${feminine_names[1]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[1]} and your Akan name is ${female_names[1]}`;
         }else if(day_of_week = 2 || day_of_week == -2)
-            result_data.innerHTML = `The day of the week is ${weekdays[2]} and your Akan name is ${feminine_names[2]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[2]} and your Akan name is ${female_names[2]}`;
         }else if(day_of_week = 3 || day_of_week == -3){
-            result_data.innerHTML = `The day of the week is ${weekdays[3]} and your Akan name is ${feminine_names[3]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[3]} and your Akan name is ${female_names[3]}`;
         }else if(day_of_week = 4 || day_of_week == -4){
-            result_data.innerHTML = `The day of the week is ${weekdays[4]} and your Akan name is ${feminine_names[4]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[4]} and your Akan name is ${female_names[4]}`;
         }else if(day_of_week = 5 || day_of_week == -5){
-            result_data.innerHTML = `The day of the week is ${weekdays[5]} and your Akan name is ${feminine_names[5]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[5]} and your Akan name is ${female_names[5]}`;
         }else if(day_of_week = 6 || day_of_week == -6){
-            result_data.innerHTML = `The day of the week is ${weekdays[6]} and your Akan name is ${feminine_names[6]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[6]} and your Akan name is ${female_names[6]}`;
         }else if(day_of_week = 7 || day_of_week == -7){
-            result_data.innerHTML = `The day of the week is ${weekdays[7]} and your Akan name is ${feminine_names[7]}`;
+            result_data.innerHTML = `The day of the week is ${weekdays[7]} and your Akan name is ${female_names[7]}`;
         }
     }
